df71d3a7b81d15bd55b3c8eaacc1d303c6d29a23
Vitaliy Bibaev <roenke54@gmail.com>
2018-12-29 11:59:31 +0700

    Add project-specific files from .idea

---
+++ .idea/fileTemplates/internal/C Header File.h
@@ -0,0 +1,7 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+#parse("C File Header.h")
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+
+#[[#endif]]# //${INCLUDE_GUARD}
---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,15 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+#parse("C File Header.h")
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+
+${NAMESPACES_OPEN}
+
+class ${NAME} {
+
+};
+
+${NAMESPACES_CLOSE}
+
+#[[#endif]]# //${INCLUDE_GUARD}
