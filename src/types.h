38fe957c1f3d41c4d864fc457749c7b303d96cf0
Vitaliy Bibaev <roenke54@gmail.com>
2018-12-29 11:50:47 +0700

    Add copyright headers

--- src/types.h
+++ src/types.h
@@ -1,6 +1,4 @@
-//
-// Created by Vitaliy.Bibaev on 28-Jun-18.
-//
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 #ifndef NATIVE_MEMORY_AGENT_TYPES_H
 #define NATIVE_MEMORY_AGENT_TYPES_H
